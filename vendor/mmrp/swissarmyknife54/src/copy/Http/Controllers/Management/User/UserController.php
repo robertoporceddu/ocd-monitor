db136c80c55f24ca320e3672f1e66c73a443800a
Roberto Porceddu <robertoporceddu@gmail.com>
2020-04-09 13:30:58 +0200

    remove vendor

--- vendor/mmrp/swissarmyknife54/src/copy/Http/Controllers/Management/User/UserController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Http\Controllers\Management\User;
-
-class UserController extends \Mmrp\Swissarmyknife\Controller\Rbac\UserController
-{
-
-}
--- vendor/mmrp/swissarmyknife54/src/copy/resources/views/layouts/partials/controlsidebar.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Control Sidebar -->
-<aside id="right-sidebar" class="control-sidebar control-sidebar-light">
-    @yield('control-sidebar')
-
-</aside>
-<!-- /.control-sidebar
-
-<!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
-<div class='control-sidebar-bg'style="position: fixed; height: auto;"></div>
