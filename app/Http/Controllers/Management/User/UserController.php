77863be9e1658836f310e5574307a56bfb1c1370
Roberto Porceddu <robertoporceddu@gmail.com>
2020-04-17 17:00:17 +0200

    bugfix usercontroller

--- app/Http/Controllers/Management/User/UserController.php
+++ app/Http/Controllers/Management/User/UserController.php
@@ -1,8 +1,31 @@
 <?php
 
 namespace App\Http\Controllers\Management\User;
+use Illuminate\Http\Request;
 
 class UserController extends \Mmrp\Swissarmyknife\Controller\Rbac\UserController
 {
+    protected function beforeSave(Request $request, $id = NULL)
+    {
+        if (is_null($id)) {
+            $this->password = str_random(32);
+            $this->input['password'] = bcrypt($this->password);
+            $this->current_action = 'insert';
+        } else {
+            if(isset($this->input['password'])) {
+                $this->input['password'] = bcrypt($this->input['password']);
+            }
+        }
 
+        unset($this->input['password_confirmation']);
+
+        if (!empty($this->input['profile_image'])) {
+            $this->input['profile_image'] = 'data:' . $this->input['profile_image']->getMimeType() . ';base64,' . base64_encode(file_get_contents($this->input['profile_image']));
+        }
+
+        if(isset($this->input['roles'])) {
+            $this->roles = $this->input['roles'];
+            unset($this->input['roles']);
+        }
+    }
 }
